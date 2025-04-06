1465cc5e8f607d9f63b942f6253e48057d9de584
farhanT23 <farhan.tanvir@brainstation-23.com>
2025-04-06 11:54:27 +0600

    feat: implement hidden password input

--- functions.php
+++ functions.php
@@ -1,1 +1,22 @@
 <?php
+
+function hidden_input($prompt = 'Enter password: ') {
+    if (strncasecmp(PHP_OS, 'WIN', 3) == 0) {
+        // Windows
+        echo $prompt;
+        $vbscript = sys_get_temp_dir() . 'prompt_password.vbs';
+        file_put_contents($vbscript, 'wscript.echo(InputBox("' . addslashes($prompt) . '", "", ""))');
+        $command = "cscript //nologo " . escapeshellarg($vbscript);
+        $password = rtrim(shell_exec($command));
+        unlink($vbscript);
+        return $password;
+    } else {
+        // Unix-like
+        echo $prompt;
+        system('stty -echo');
+        $password = rtrim(fgets(STDIN), "\n");
+        system('stty echo');
+        echo "\n";
+        return $password;
+    }
+}
